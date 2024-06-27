4c72c731f858540d8b038cb85847c0599903ff7f
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-27 11:29:01 +0600

    slice  created

---
+++ src/redux/feature/counterSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+export interface CounterState {
+    count: number
+}
+
+
+const initialState: CounterState = {
+    count: 0
+}
+
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.count + 1
+        }
+    }
+})
+
+
+
+
+export const { increment } = counterSlice.actions
+export default counterSlice
